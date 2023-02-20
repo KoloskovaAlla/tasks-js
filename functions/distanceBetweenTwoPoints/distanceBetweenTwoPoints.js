d494a9b6e49eca459f2848ab963b4ddc3e9217a9
AllaKoloskova <91485080+KoloskovaAlla@users.noreply.github.com>
2023-02-20 20:09:21 +0300

    Delete distanceBetweenTwoPoints.js

--- functions/distanceBetweenTwoPoints/distanceBetweenTwoPoints.js
+++
@@ -1,5 +0,0 @@
-export const distanceBetweenTwoPoints = (pointA = {}, pointB = {}) => {
-  const distanceX = pointB.x - pointA.x;
-  const distanceY = pointB.y - pointA.y; 
-  return Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
-}
