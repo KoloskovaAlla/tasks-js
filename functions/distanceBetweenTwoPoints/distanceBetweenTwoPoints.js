d9af45f3b3b24129e10237c4c7f6de89809dde40
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2023-02-20 06:50:47 +0300

    Fixing the indentetion

--- functions/distanceBetweenTwoPoints/distanceBetweenTwoPoints.js
+++ functions/distanceBetweenTwoPoints/distanceBetweenTwoPoints.js
@@ -1,4 +1,4 @@
-export const distanceBetweenTwoPoints = ( pointA={}, pointB={} ) => {
+export const distanceBetweenTwoPoints = (pointA = {}, pointB = {}) => {
   const distanceX = pointB.x - pointA.x;
   const distanceY = pointB.y - pointA.y; 
   return Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
