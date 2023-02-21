30e48fb62a5e93f83038498d066bc2a06d4a5f99
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2023-02-21 09:42:24 +0300

    WIP powersOfTwo

--- functions/powersOfTwo/powersOfTwo.js
+++ functions/powersOfTwo/powersOfTwo.js
@@ -1,9 +1,14 @@
-export const powersOfTwo = (n) => {
-  const arr = []
-  
-  for (let i=0; i<=n; i++) {   
-    arr.push(Math.pow(2, i))
+/** 
+ * @param {number} indicator
+ * @returns {array}
+ */
+
+export const powersOfTwo = (indicator) => {
+  const arr = [];
+
+  for (let index = 0; index <= indicator; index++) {
+    arr.push(Math.pow(2, index));
   };
 
-  return arr
-}
+  return arr;
+};
--- functions/powersOfTwo/powersOfTwo.test.js
+++ functions/powersOfTwo/powersOfTwo.test.js
@@ -1,16 +1,26 @@
-import { powersOfTwo } from './powersOfTwo.js'; 
- 
-const dataForTests = { 
-  test1: { 
-    power: 5, 
-    expectedArray: [ 1, 2, 4, 8, 16, 32 ] 
-  } 
-}; 
- 
-describe('powersOfTwo:', () => { 
-  it('Unit test 1', () => { 
-    const { power, expectedArray } = dataForTests.test1; 
-    const powersOfTwoArr = powersOfTwo(power) 
-    expect(powersOfTwoArr).toEqual(expectedArray); 
-  }); 
+import { powersOfTwo } from './powersOfTwo.js';
+
+const dataForTests = {
+  test1: {
+    power: 5,
+    expectedArray: [1, 2, 4, 8, 16, 32]
+  },
+  test2: {
+    power: 7,
+    expectedArray: [1, 2, 4, 8, 16, 32, 64, 128]
+  }
+};
+
+describe('powersOfTwo:', () => {
+  it('Unit test 1', () => {
+    const { power, expectedArray } = dataForTests.test1;
+    const powersOfTwoArr = powersOfTwo(power);
+    expect(powersOfTwoArr).toEqual(expectedArray);
+  });
+
+  it('Unit test 2', () => {
+    const { power, expectedArray } = dataForTests.test2;
+    const powersOfTwoArr = powersOfTwo(power);
+    expect(powersOfTwoArr).toEqual(expectedArray);
+  });
 });
