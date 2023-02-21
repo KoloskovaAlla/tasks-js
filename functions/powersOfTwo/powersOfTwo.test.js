a75764a337d00a19cd3257ee2ae2beab691f3840
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2023-02-21 11:29:24 +0300

    Fixing

--- functions/powersOfTwo/powersOfTwo.test.js
+++ functions/powersOfTwo/powersOfTwo.test.js
@@ -2,25 +2,25 @@ import { powersOfTwo } from './powersOfTwo.js';
 
 const dataForTests = {
   test1: {
-    power: 5,
+    indicator: 5,
     expectedArray: [1, 2, 4, 8, 16, 32]
   },
   test2: {
-    power: 7,
+    indicator: 7,
     expectedArray: [1, 2, 4, 8, 16, 32, 64, 128]
   }
 };
 
 describe('powersOfTwo:', () => {
   it('Unit test 1', () => {
-    const { power, expectedArray } = dataForTests.test1;
-    const powersOfTwoArr = powersOfTwo(power);
+    const { indicator, expectedArray } = dataForTests.test1;
+    const powersOfTwoArr = powersOfTwo(indicator);
     expect(powersOfTwoArr).toEqual(expectedArray);
   });
 
   it('Unit test 2', () => {
-    const { power, expectedArray } = dataForTests.test2;
-    const powersOfTwoArr = powersOfTwo(power);
+    const { indicator, expectedArray } = dataForTests.test2;
+    const powersOfTwoArr = powersOfTwo(indicator);
     expect(powersOfTwoArr).toEqual(expectedArray);
   });
 });
