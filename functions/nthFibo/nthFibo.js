9941b839a6a5d704e3e7b5f9e91229707ef3f091
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2023-02-23 19:37:42 +0300

    Refactoring

--- functions/nthFibo/nthFibo.js
+++ functions/nthFibo/nthFibo.js
@@ -4,17 +4,12 @@
  */
 
 export const nthFibo = (place) => {
-  let fibo = [0, 1];
-
-  if (place===1) return 0;
-  if (place==2) return 1;
-  
-  let currentNumberFibo;
+  const fiboNums = [0, 1];  
   
   for (let index = 2; index <= place; index++) {
-    currentNumberFibo = fibo[index-2]+fibo[index-1];
-    fibo.push(currentNumberFibo);
-  
-    if (index+1===place) return currentNumberFibo;
+    const fiboNum = fiboNums[index - 2] + fiboNums[index - 1];
+    fiboNums.push(fiboNum);
   };
+
+  return fiboNums[place - 1];
 };
