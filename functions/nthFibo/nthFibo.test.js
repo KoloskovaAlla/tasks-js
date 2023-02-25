ac9bc489b1d6c82d5afd7740b86cc5c476f853b7
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2023-02-25 18:43:45 +0300

    Refactoring name of constants

--- functions/nthFibo/nthFibo.test.js
+++ functions/nthFibo/nthFibo.test.js
@@ -3,34 +3,34 @@ import { nthFibo } from './nthFibo';
 const dataForTests = {
   test1: {
     place: 4,
-    expectedNumber: 2
+    expectedFiboNum: 2
   },
   test2: {
     place: 8,
-    expectedNumber: 13
+    expectedFiboNum: 13
   },
   test3: {
     place: 13,
-    expectedNumber: 144
+    expectedFiboNum: 144
   }
 };
 
 describe('numberFibonacci:', () => {
   it('Unit test 1', () => {
-    const { place, expectedNumber } = dataForTests.test1;  
-    const numberFibonacci = nthFibo(place);
-    expect(numberFibonacci).toEqual(expectedNumber);
+    const { place, expectedFiboNum } = dataForTests.test1;  
+    const fiboNum = nthFibo(place);
+    expect(fiboNum).toEqual(expectedFiboNum);
   });
 
   it('Unit test 2', () => {
-    const { place, expectedNumber } = dataForTests.test2;  
-    const numberFibonacci = nthFibo(place);
-    expect(numberFibonacci).toEqual(expectedNumber);
+    const { place, expectedFiboNum } = dataForTests.test2;  
+    const fiboNum = nthFibo(place);
+    expect(fiboNum).toEqual(expectedFiboNum);
   });
 
   it('Unit test 3', () => {
-    const { place, expectedNumber } = dataForTests.test3;  
-    const numberFibonacci = nthFibo(place);
-    expect(numberFibonacci).toEqual(expectedNumber);
+    const { place, expectedFiboNum } = dataForTests.test3;  
+    const fiboNum = nthFibo(place);
+    expect(fiboNum).toEqual(expectedFiboNum);
   });
 });
