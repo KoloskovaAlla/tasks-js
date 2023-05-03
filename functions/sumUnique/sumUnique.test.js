6e492033be30c75ccfa1f9ec62c785dee03f6b39
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2023-05-03 20:45:12 +0300

    Test

---
+++ functions/sumUnique/sumUnique.test.js
@@ -0,0 +1,17 @@
+import { sumUnique } from './sumUnique.js';
+
+const dataForTests = {
+  test1: {
+  list: [3, 4, 6, 3],
+  expectedSum: 10,
+  }
+}
+
+describe('classNames:', () => {
+  it('Unit test 1', () => {
+    const { list, expectedSum } = dataForTests.test1;
+    
+    const sum = sumUnique(list);
+    expect(sum).toEqual(expectedSum);
+  });
+});
