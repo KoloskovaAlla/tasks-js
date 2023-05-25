99c4c2f4aaf5e72493df4ee5af2eabcdba93539b
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2023-05-25 21:09:31 +0300

    Printer Errors

---
+++ functions/printerErrors/printerErrors.js
@@ -0,0 +1,8 @@
+const printerErrors = (string) => {
+  let countErrors = 0;
+  for (let index = 0; index < string.length; index++) if (string[index] > 'm' || string[index] < 'a')  countErrors++;
+  console.log(`${countErrors} / ${string.length}`);
+  return (`${countErrors} / ${string.length}`);
+};
+
+printerErrors("aaaxbbbbyyhwawiwjjjwwm");
