e0b7e3471ca8896c4a3be34b625bf743dbeaff5b
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2023-06-20 21:36:01 +0300

    Function rotate for rotate symbols in string

---
+++ functions/rotate/rotate.js
@@ -0,0 +1,12 @@
+export const rotate = (text) => { 
+  if (text.length === 0) { 
+    return []; 
+  }; 
+  let rotations = []; 
+ 
+  for (let index = 1; index <= text.length; index++) { 
+    let rotated = text.slice(index) + text.slice(0, index); 
+    rotations.push(rotated); 
+  }; 
+  return rotations; 
+};
