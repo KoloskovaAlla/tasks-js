a94e4503e3d31eca2770db77022bad7e4f5d8f8c
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2023-08-09 18:02:29 +0300

    WIP

--- functions/gameWithChanging/gameWithChanging.js
+++ functions/gameWithChanging/gameWithChanging.js
@@ -5,6 +5,8 @@ const reader = readline.createInterface({
     output: process.stdout
 });
 
+const pairs = [];
+
 const canTransform = (s1, s2) => {
     if (s1.length !== s2.length) {
         return "NO";
@@ -13,26 +15,31 @@ const canTransform = (s1, s2) => {
     const transformations = new Map();
 
     for (let i = 0; i < s1.length; i++) {
-        if (!transformations.has(s1[i]) && !transformations.has(s2[i])) {
+        if (!transformations.has(s1[i])) {
             transformations.set(s1[i], s2[i]);
+        } else if (transformations.get(s1[i]) !== s2[i]) {
+            return "NO"; // Если замена не соответствует правилам, возвращаем "NO"
+        }
+
+        if (!transformations.has(s2[i])) {
             transformations.set(s2[i], s1[i]);
-        } else if (transformations.get(s1[i]) !== s2[i] || transformations.get(s2[i]) !== s1[i]) {
-            return "NO";
+        } else if (transformations.get(s2[i]) !== s1[i]) {
+            return "NO"; // Если замена не соответствует правилам, возвращаем "NO"
         }
     }
 
-    return "YES";
+    return "YES"; // Если все замены прошли успешно, возвращаем "YES"
 }
 
 let count = 0;
+
 let currentPair = [];
-const pairs = [];
+
 reader.on('line', (line) => {
     if (count === 0) {
         count = parseInt(line);
     } else {
         currentPair.push(line.trim());
-        console.log('вывод в консоль - ' + currentPair);
         if (currentPair.length === 2) {
             pairs.push(currentPair);
             currentPair = [];
