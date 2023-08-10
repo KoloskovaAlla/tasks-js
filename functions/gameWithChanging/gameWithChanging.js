238fddb46b3a1565bb4ca508a41669e80467b9d0
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2023-08-10 13:15:12 +0300

    Refactored gameWithChanging

--- functions/gameWithChanging/gameWithChanging.js
+++ functions/gameWithChanging/gameWithChanging.js
@@ -7,28 +7,29 @@ const reader = readline.createInterface({
 
 const pairs = [];
 
-const canTransform = (s1, s2) => {
-    if (s1.length !== s2.length) {
-        return "NO";
+const canTransform = (string1, string2) => {
+    //если строки разной длины, дальше можно не сравнивать и сразу вернуть NO
+    if (string1.length !== string2.length) return 'NO';
+
+    // Объект, где будут храниться пары символов
+    const transformations = {};
+
+    // Цикл для посимвольного прохождения по строкам
+    for (let index = 0; index < string1.length; index++) {
+        const currentChar1 = string1[index]; // текущий символ из первой строки
+        const currentChar2 = string2[index]; // текущий символ из второй строки
+
+        //Если у символа из первой строки еще нет пары, добавляем пару
+        if (!transformations[currentChar1]) transformations[currentChar1] = currentChar2;
+
+        //Если у символа из второй строки еще нет пары, добавляем ему пару
+        if (!transformations[currentChar2]) transformations[currentChar2] = currentChar1;
+
+        //Является ли пара взаимной
+        if (transformations[currentChar1] !== currentChar2 || transformations[currentChar2] !== currentChar1) return 'NO';
     }
 
-    const transformations = new Map();
-
-    for (let i = 0; i < s1.length; i++) {
-        if (!transformations.has(s1[i])) {
-            transformations.set(s1[i], s2[i]);
-        } else if (transformations.get(s1[i]) !== s2[i]) {
-            return "NO"; // Если замена не соответствует правилам, возвращаем "NO"
-        }
-
-        if (!transformations.has(s2[i])) {
-            transformations.set(s2[i], s1[i]);
-        } else if (transformations.get(s2[i]) !== s1[i]) {
-            return "NO"; // Если замена не соответствует правилам, возвращаем "NO"
-        }
-    }
-
-    return "YES"; // Если все замены прошли успешно, возвращаем "YES"
+    return 'YES';
 }
 
 let count = 0;
@@ -47,8 +48,9 @@ reader.on('line', (line) => {
         }
 
         if (count === 0) {
-            const result = pairs.map(pair => canTransform(pair[0], pair[1]));
-            result.forEach(res => console.log(res));
+            // const result = pairs.map(pair => canTransform(pair[0], pair[1]));
+            // result.forEach(res => console.log(res));
+            pairs.map((pair) => console.log(canTransform(pair[0], pair[1])))
             reader.close();
         }
     }
