cdf5d145366b753839409c392a074e1c633fbc2b
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2023-08-14 20:35:10 +0300

    Replace return

--- functions/gameWithChanging/gameWithChanging.js
+++ functions/gameWithChanging/gameWithChanging.js
@@ -6,33 +6,29 @@ const reader = readline.createInterface({
 });
 
 const pairs = [];
+// if (str1.length !== str2.length || !/^[a-zA-Z]+$/.test(str1)) return 'NO';
 
 const canTransform = (str1, str2) => {
-    //если строки разной длины, дальше можно не сравнивать и сразу вернуть NO
-    if (str1.length !== str2.length || !/^[a-zA-Z]+$/.test(str1)) return 'NO';
-
-    // Объект, где будут храниться пары символов
     const swap = {};
 
-    // Цикл для посимвольного прохождения по строкам
+    const hasMismatch = (char1, char2) => {
+        return swap[char1] !== char2;
+    };
+    let hasNonMutualPair;
+   
     const str1Chars = str1.split('');
+
     str1Chars.forEach((str1Char, index) => {
         const str2Char = str2[index];
+
+        if (!swap[str1Char]) swap[str1Char] = str2Char;
+        if (!swap[str2Char]) swap[str2Char] = str1Char; 
+
+        const hasMismatchFromStr1 =  hasMismatch(str1Char, str2Char); 
+        const hasMismatchFromStr2 =  hasMismatch(str2Char, str1Char);    
+        hasNonMutualPair = hasMismatchFromStr1 || hasMismatchFromStr2;       
     });
-    for (let index = 0; index < str1.length; index++) {
-        const currentChar1 = str1[index]; // текущий символ из первой строки
-        const currentChar2 = str2[index]; // текущий символ из второй строки
-
-        //Если у символа из первой строки еще нет пары, добавляем пару
-        if (!swap[currentChar1]) swap[currentChar1] = currentChar2;
-
-        //Если у символа из второй строки еще нет пары, добавляем ему пару
-        if (!swap[currentChar2]) swap[currentChar2] = currentChar1;
-
-        //Является ли пара взаимной
-        if (swap[currentChar1] !== currentChar2 || swap[currentChar2] !== currentChar1) return 'NO';
-    };
-
+    if (hasNonMutualPair) return 'NO';
     return 'YES';
 };
 
