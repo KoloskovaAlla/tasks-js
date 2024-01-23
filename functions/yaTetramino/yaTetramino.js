3be1b94a16af3f33eaf93cfa7a68b5af97fc9649
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2024-01-23 18:16:13 +0300

    made 4 variants of tetramino

--- functions/yaTetramino/yaTetramino.js
+++ functions/yaTetramino/yaTetramino.js
@@ -1,14 +1,14 @@
 let count = 0;
 
 const board = [
-  '........',
-  '........',
-  '........',
-  '........',
-  '........',
-  '........',
-  '........',
-  '........'
+  '....****',
+  '....****',
+  '....****',
+  '....****',
+  '****....',
+  '****....',
+  '****....',
+  '****....'
 ];
 
 
@@ -32,6 +32,26 @@ const canBeTetramino2 = (board, i, j) => {
   return canBeTetramino;
 };
 
+//вариант, когда тетрамино, как буква Т повернута по часовой стрелке на 180 градусов
+const canBeTetramino3 = (board, i, j) => {
+  const canBeTetramino =
+    board[i][j] === '.' &&
+    board[i + 1][j - 1] === '.' &&
+    board[i + 1][j] === '.' &&
+    board[i + 1][j + 1] === '.';
+  return canBeTetramino;
+};
+
+//вариант, когда тетрамино, как буква Т повернута по часовой стрелке на 270 градусов
+const canBeTetramino4 = (board, i, j) => {
+  const canBeTetramino =
+    board[i][j] === '.' &&
+    board[i + 1][j] === '.' &&
+    board[i + 1][j + 1] === '.' &&
+    board[i + 2][j] === '.';
+  return canBeTetramino;
+};
+
 // Это цикл изменения строк и колонок, когда тетрамино буквой T
 for (let row = 0; row < 7; row++) {
   for (let column = 0; column < 6; column++) {
@@ -39,11 +59,26 @@ for (let row = 0; row < 7; row++) {
   };
 };
 
-// Это цикл изменения строк и колонок, когда тетрамино буквой T
-for (let row = 1; row < 8; row++) {
-  for (let column = 0; column < 6; column++) {
+// Это цикл изменения строк и колонок, когда Т повернута по часовой стрелке на 90 градусов
+for (let row = 0; row < 6; row++) {
+  for (let column = 1; column < 8; column++) {
     if (canBeTetramino2(board, row, column)) count++;
   };
 };
+
+// Это цикл изменения строк и колонок, когда Т повернута по часовой стрелке на 180 градусов
+for (let row = 0; row < 7; row++) {
+  for (let column = 0; column < 7; column++) {
+    if (canBeTetramino3(board, row, column)) count++;
+  };
+};
+
+// Это цикл изменения строк и колонок, когда Т повернута по часовой стрелке на 270 градусов
+for (let row = 0; row < 6; row++) {
+  for (let column = 0; column < 7; column++) {
+    if (canBeTetramino4(board, row, column)) count++;
+  };
+};
+
 
 console.log(count);
