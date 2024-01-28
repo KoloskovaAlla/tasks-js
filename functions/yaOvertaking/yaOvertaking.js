b74073b15c56364f4ccdd2e052514ecbc62160ba
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2024-01-28 23:00:15 +0300

    Added BigInt

--- functions/yaOvertaking/yaOvertaking.js
+++ functions/yaOvertaking/yaOvertaking.js
@@ -3,24 +3,29 @@ import readline from 'readline';
 
 const countOvertakes = (n, t, s, speeds) => {
   let overtaking = 0;
+  let bigOvertaking = BigInt(0);
   const fractPart0 = speeds[0] / s * t % 1;
   const bigSpeed0 = BigInt(speeds[0]);
 
   for (let i = 1; i < n; i++) {
     if (speeds[0] > speeds[i]) {
-      const fractPartI = speeds[i] % 1;
-
-      const dif = (speeds[0] - speeds[i]) / s * t
-      const bigSpeedI = BigInt(speeds[i]);
-      const bigSum = BigInt(bigSpeed0 - bigSpeedI);
-      const bigS = BigInt(s);
-      const bigT = BigInt(t);
-      const bigDif = bigSum / bigS * bigT;
-      if (fractPart0 === fractPartI) overtaking += dif - 1;
-      else overtaking += Math.floor(dif);
+      const fractPartI = speeds[i] / s * t % 1;
+      
+      const dif =(speeds[0] - speeds[i]) / s * t
+      
+       const bigSpeedI = BigInt(speeds[i]);
+       const bigSum= BigInt(bigSpeed0 -  bigSpeedI);
+       const bigDif = bigSum / BigInt(s) * BigInt(t);
+      
+      overtaking += dif - 1;
+      bigOvertaking += bigDif - BigInt(1);
+      
+      
+      // else overtaking += Math.floor(dif); 
     };
-  };
-  return (overtaking);
+  }
+      const result = Number.isInteger(overtaking) ? overtaking : bigOvertaking;
+  return result;
 };
 
 const reader = readline.createInterface({
