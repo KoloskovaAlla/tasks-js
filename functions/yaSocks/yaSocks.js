392af4c175ed1f285870ad267c2c546bf59ae700
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2024-10-28 11:43:42 +0300

    Done task

--- functions/yaSocks/yaSocks.js
+++ functions/yaSocks/yaSocks.js
@@ -16,64 +16,75 @@ rl.on("line", (line) => {
 });
 
 const findKit = (a, b, c, d) => {
-    const attemtsForBlueTShirts = b + 1;
-    const attemtsForBlueSocks = d + 1;
-    const blueKit = attemtsForBlueTShirts + attemtsForBlueSocks;
+  const attemtsForBlueTShirts = b + 1;
+  const attemtsForBlueSocks = d + 1;
+  const blueKit = attemtsForBlueTShirts + attemtsForBlueSocks;
 
-    const attemtsForRedTShirt = a + 1;
-    const attemtsForRedSocks = c + 1;
-    const redKit = attemtsForRedTShirt + attemtsForRedSocks;
+  const attemtsForRedTShirt = a + 1;
+  const attemtsForRedSocks = c + 1;
+  const redKit = attemtsForRedTShirt + attemtsForRedSocks;
 
-     if (a === b)  {
-      const minSocks = attemtsForBlueSocks < attemtsForRedSocks ? attemtsForBlueSocks : attemtsForRedSocks;
-      const minTShirts = attemtsForRedTShirt < attemtsForBlueTShirts ? attemtsForRedTShirt : attemtsForBlueTShirts;
-      const variantForOneTShirt = 1 + minSocks;
-      const variantForOnePairSocks =  minTShirts + 1; 
-      if (variantForOneTShirt < variantForOnePairSocks) {
-        // console.log('вариант 1 + ....')
-        // 1 + ...
-        if (attemtsForBlueSocks < attemtsForRedSocks) return (1 + ' ' + attemtsForBlueSocks)
-        else return (1 + ' ' + attemtsForRedSocks);
-      }
-        else {
-          // console.log('вариант ... + 1')
-          // ...+1
-          if (attemtsForBlueTShirts < attemtsForRedSocks) return (attemtsForBlueTShirts + ' ' + 1)
-            else return (attemtsForRedSocks + ' ' + 1);
-        }        
-     }; 
-    if (a === 0) return (1 + ' ' + attemtsForRedSocks);
-    if (b === 0) return (1 + ' ' + attemtsForBlueSocks);
-    if (c === 0) return (attemtsForRedTShirt + ' ' + 1);
-    if (d === 0) return (attemtsForBlueTShirts + ' ' + 1);
-
-    if (attemtsForBlueTShirts - attemtsForRedTShirt === 1)  return (attemtsForBlueTShirts + ' ' + 1);
-    if (attemtsForRedTShirt - attemtsForBlueTShirts === 1)  return (attemtsForRedTShirt + ' ' + 1);    
-
-    // console.log('для синих ' + attemtsForBlueTShirts + ' и ' + attemtsForBlueSocks + ', сумма ' + blueKit);
-    // console.log('для красных ' + attemtsForRedTShirt + 'и ' + attemtsForRedSocks+ ', сумма ' + redKit);
-
-    if (blueKit < redKit) {
-      // console.log('синих меньше');
-      let anotherVarinat; 
-      if (b > a) {
-        anotherVarinat = b + 1;
-        if (anotherVarinat && anotherVarinat + 1 < blueKit) return anotherVarinat + ' ' + 1
-          else attemtsForBlueTShirts + ' ' + attemtsForBlueSocks;  
-      }
-        else return attemtsForBlueTShirts + ' ' + attemtsForBlueSocks;      
+  if (a === b) {
+    const minSocks = attemtsForBlueSocks < attemtsForRedSocks ? attemtsForBlueSocks : attemtsForRedSocks;
+    const minTShirts = attemtsForRedTShirt < attemtsForBlueTShirts ? attemtsForRedTShirt : attemtsForBlueTShirts;
+    const variantForOneTShirt = 1 + minSocks;
+    const variantForOnePairSocks = minTShirts + 1;
+    if (variantForOneTShirt < variantForOnePairSocks) {
+      // console.log('вариант 1 + ....')
+      // 1 + ...
+      if (attemtsForBlueSocks < attemtsForRedSocks) return (1 + ' ' + attemtsForBlueSocks)
+      else return (1 + ' ' + attemtsForRedSocks);
     }
-      else {
-        // console.log('попыток для красных меньше') 
-        let anotherVarinat;         
-        if (a > b) anotherVarinat = a + 1;
-        if (anotherVarinat && anotherVarinat + 1 < redKit) return anotherVarinat + ' ' + 1
-        else attemtsForRedTShirt + ' ' + attemtsForRedSocks;           
-      }        
+    else {
+      // console.log('вариант ... + 1')
+      // ...+1
+      if (attemtsForBlueTShirts < attemtsForRedSocks) return (attemtsForBlueTShirts + ' ' + 1)
+      else return (attemtsForRedSocks + ' ' + 1);
+    }
+  };
+  if (a === 0) return (1 + ' ' + attemtsForRedSocks);
+  if (b === 0) return (1 + ' ' + attemtsForBlueSocks);
+  if (c === 0) return (attemtsForRedTShirt + ' ' + 1);
+  if (d === 0) return (attemtsForBlueTShirts + ' ' + 1);
+
+  if (attemtsForBlueTShirts - attemtsForRedTShirt === 1) return (attemtsForBlueTShirts + ' ' + 1);
+  if (attemtsForRedTShirt - attemtsForBlueTShirts === 1) return (attemtsForRedTShirt + ' ' + 1);
+
+  // console.log('для синих ' + attemtsForBlueTShirts + ' и ' + attemtsForBlueSocks + ', сумма ' + blueKit);
+  // console.log('для красных ' + attemtsForRedTShirt + 'и ' + attemtsForRedSocks + ', сумма ' + redKit);
+
+  if (blueKit < redKit) {
+    // console.log('синих меньше');
+    const maxTShirts = a > b ? a + 1 : b + 1;
+    const variantWithMaxTshirts = maxTShirts + 1;
+
+    const maxSocks = c > d ? c + 1 : d + 1;
+    const variantWithMaxSocks = maxSocks + 1;
+
+    if (blueKit < variantWithMaxTshirts && blueKit < variantWithMaxSocks) return attemtsForBlueTShirts + ' ' + attemtsForBlueSocks;
+    if (variantWithMaxTshirts < blueKit && variantWithMaxTshirts < variantWithMaxSocks) return maxTShirts + ' ' + 1;
+    if (variantWithMaxSocks < variantWithMaxTshirts && variantWithMaxSocks < blueKit) return 1 + ' ' + maxSocks;
+
+  }
+  else {
+    // console.log('попыток для красных меньше')
+
+    const maxTShirts = a > b ? a + 1 : b + 1;
+    const variantWithMaxTshirts = maxTShirts + 1;
+
+    const maxSocks = c > d ? c + 1 : d + 1;
+    const variantWithMaxSocks = maxSocks + 1;
+
+    if (redKit < variantWithMaxTshirts && redKit < variantWithMaxSocks) return attemtsForRedTShirt + ' ' + attemtsForRedSocks;
+    if (variantWithMaxTshirts < redKit && variantWithMaxTshirts < variantWithMaxSocks) return maxTShirts + ' ' + 1;
+    if (variantWithMaxSocks < variantWithMaxTshirts && variantWithMaxSocks < redKit) return 1 + ' ' + maxSocks;
+
+
+  }
 }
 
 rl.on("close", () => {
-  const [a, b, c, d] = input; 
+  const [a, b, c, d] = input;
 
   console.log(findKit(a, b, c, d));
 });
