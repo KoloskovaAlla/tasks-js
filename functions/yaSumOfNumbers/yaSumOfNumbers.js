355e681c27a55f3d94643b66fde398fe639c53b3
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2024-11-02 17:55:05 +0300

    Second task Prefiks sum

--- functions/yaSumOfNumbers/yaSumOfNumbers.js
+++ functions/yaSumOfNumbers/yaSumOfNumbers.js
@@ -31,15 +31,35 @@ rl.on("close", () => {
 
     prefSumArray[0] = 0;
     let prefSum = 0;
+    const prefixSumsCount = { 0: 1 }; // Инициализация с нулевой префиксной суммой
+
+
+    // for (let index = 1; index < prefSumArray.length; index++) {
+    //   prefSum += carsArray[index - 1];
+    //   prefSumArray[index] = prefSum;
+    // }
+
+    let countSets = 0;
 
     for (let index = 1; index < prefSumArray.length; index++) {
-      prefSum += carsArray[index - 1];
+      prefSum += carsArray[index - 1]; // Обновляем префиксную сумму
       prefSumArray[index] = prefSum;
+
+      // Проверяем, есть ли префиксная сумма S - K
+      const target = prefSum - luckySum;
+      if (prefixSumsCount[target]) {
+        countSets += prefixSumsCount[target]; // Увеличиваем счетчик
+      }
+
+      // Обновляем количество текущей префиксной суммы в хеш-таблице
+      prefixSumsCount[prefSum] = (prefixSumsCount[prefSum] || 0) + 1;
     }
 
-    console.log(prefSumArray);
+    // console.log(prefSumArray);
+    // console.log("----------------------");
+    // console.log(prefixSumsCount);
 
-    // console.log(numberOfSets);
+    console.log(countSets);
   };
 
   sumOfNumbers(numberOfCars, luckySum, carsArray);
