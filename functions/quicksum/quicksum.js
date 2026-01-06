291eadefdf29db4d32bed48fa3345657082153a8
koloskova-ay <koloskova-ay@m13.ru>
2026-01-06 21:47:54 +0300

    Quick sum

---
+++ functions/quicksum/quicksum.js
@@ -0,0 +1,20 @@
+const quicksum = (packet) => {
+    result = 0;
+    for (let i = 0; i < packet.length; i++) {   
+        const charCode = packet.charCodeAt(i)
+        // console.log(charCode)
+        if (packet[i] === ' ') result += 0
+            else if (charCode < 65 || charCode > 90) return 0;
+                else result += (i + 1) *  (packet.charCodeAt(i) - 65 + 1)
+
+    }
+    return result;    
+}
+
+// ACM
+console.log(quicksum('ACM')) // 46
+console.log(quicksum('A C M')) // 75
+console.log(quicksum('MID CENTRAL')) // 650
+console.log(quicksum('???')) // 0
+console.log(quicksum('234 234 WEF ASDF AAA 554211 ????')) // 0
+
