5b1b899627cb63e2d4a7450bba60b899cf912cfa
koloskova-ay <koloskova-ay@m13.ru>
2026-02-04 22:18:38 +0300

    fib

--- functions/fib/fib.js
+++ functions/fib/fib.js
@@ -1,3 +1,15 @@
 function solution(first,second) {
-  return [  ];
+  let prev; 
+ 
+  while (true) {
+    prev = second - first;
+   
+    if (prev < 0 || second < first || first < prev) {
+      return [first, second]
+    }
+      else {
+       second = first;
+       first = prev;
+      }     
+  }
 }
